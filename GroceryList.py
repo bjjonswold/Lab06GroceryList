6d688008e8694410ff7ba160468f16c9d7d36b20
bjjonswold <bjjonswold@gmail.com>
2023-02-14 14:12:29 -0700

    Turn it in

--- GroceryList.py
+++ GroceryList.py
@@ -2,7 +2,8 @@
 1. The output would be a list of numbers within the range of num. 
     For itemFetching(6), the output would be [0, 1, 2, 3, 4, 5, "got them all!"]
 2. There would be a "Got them all!" after each number in the list
-    For itemFetching(6), the output would be [0, "got them all!", 1, "got them all!", 2, "got them all!", 3, "got them all!", 4, "got them all!", 5, "got them all!"]
+    For itemFetching(6), the output would be:
+    [0, "got them all!", 1, "got them all!", 2, "got them all!", 3, "got them all!", 4, "got them all!", 5, "got them all!"]
 3. The returned output would be a list of strings and there would also be several printed statements. The output would be:
     I'm not sure why she uses odd numbers for bread...
     I can't even read her handwriting for this one...
@@ -11,7 +12,7 @@
     Oh good, vegetables
     I'm not sure why she uses odd numbers for bread...
     Not sure why there are prices in here.
-    
+
     Returned Value: 
     ['bread', 'peas', 'snacks(??)', 'carrots', 'bread', 'snacks(??)']
 4. myFavoriteThings = [17, 0.2, "Phantom of the Opera"]
