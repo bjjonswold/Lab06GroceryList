b9524489dfb22ab21cbe9ef48b0f5ff826730da6
bjjonswold <bjjonswold@gmail.com>
2023-02-14 14:11:25 -0700

    Updated for Turn in

--- GroceryList.py
+++ GroceryList.py
@@ -1,8 +1,19 @@
 '''
 1. The output would be a list of numbers within the range of num. 
-    For itemFetching(6), the output would be [1, 2, 3, 4, 5, 6, "got them all!"]
+    For itemFetching(6), the output would be [0, 1, 2, 3, 4, 5, "got them all!"]
 2. There would be a "Got them all!" after each number in the list
-3. The output would be a list of strings. The output would be 
+    For itemFetching(6), the output would be [0, "got them all!", 1, "got them all!", 2, "got them all!", 3, "got them all!", 4, "got them all!", 5, "got them all!"]
+3. The returned output would be a list of strings and there would also be several printed statements. The output would be:
+    I'm not sure why she uses odd numbers for bread...
+    I can't even read her handwriting for this one...
+    Oh good, vegetables
+    Not sure why there are prices in here.
+    Oh good, vegetables
+    I'm not sure why she uses odd numbers for bread...
+    Not sure why there are prices in here.
+    
+    Returned Value: 
+    ['bread', 'peas', 'snacks(??)', 'carrots', 'bread', 'snacks(??)']
 4. myFavoriteThings = [17, 0.2, "Phantom of the Opera"]
 '''
 #input: num--number of items to get
@@ -31,6 +42,8 @@ def itemSorting(lst):
         else:
             print("I can't even read her handwriting for this one...")
     return sorted
+
+print(itemSorting([3, 7, "peas", 2.25, "carrots", 1, 5.3]))
  
 #input: timer--number of seconds in time out
 #output: countDown-- a list with the timer counting down until 0
